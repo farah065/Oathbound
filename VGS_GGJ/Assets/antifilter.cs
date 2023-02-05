6ba09b31981b1e239d0ebfce797070c5e3b0d713
Youssef El Sharkawy <yelsharkawy42@gmail.com>
2023-02-05 19:11:23 +0200

    just redid my old work

--- VGS_GGJ/Assets/antifilter.cs
+++ VGS_GGJ/Assets/antifilter.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class antifilter : MonoBehaviour
 {
     // Start is called before the first frame update
     SpriteRenderer spr;
     float vis = 1.5f;
+    public AudioSource audio;
+    bool ok = false;
     void Start()
     {
         spr = GetComponent<SpriteRenderer>();
+        StartCoroutine(makeok());
     }
 
     // Update is called once per frame
@@ -19,5 +23,12 @@ public class antifilter : MonoBehaviour
         vis -= Time.deltaTime;
         tmp.a = Mathf.Lerp(0, 1, vis);
         spr.color = tmp;
+        if (!audio.isPlaying || (ok && Input.anyKey))
+            SceneManager.LoadScene("Main Menu");
+    }
+    IEnumerator makeok()
+    {
+        yield return new WaitForSeconds(5);
+        ok = true;
     }
 }
